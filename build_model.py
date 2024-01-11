4cf2252e4d9dd4b6e2a8b84ea0aa9352266e7fea
Greg <gregory.martin.data@gmail.com>
2024-01-11 06:09:26 +0100

    Used black to lint build_model.py

--- build_model.py
+++ build_model.py
@@ -29,17 +29,14 @@ dirs = ["csvs", "graphs"]
 for dir in dirs:
     if not os.path.exists(dir):
         os.makedirs(dir)
-        
+
 # Loading Data
 url = "https://simplonline-v3-prod.s3.eu-west-3.amazonaws.com/media/file/csv/4072eb5e-e963-4a17-a794-3ea028d0a9c4.csv"
 output_path = "csvs/dataset.csv"
 wget.download(url, output_path)
-        
+
 # Cleaning Data
-df = (pd
-      .read_csv(output_path)
-      .drop_duplicates()
-)
+df = pd.read_csv(output_path).drop_duplicates()
 
 # Dump Clean Data
 df.to_csv("csvs/cleaned_dataset.csv", index=False)
@@ -52,11 +49,9 @@ X = df
 y = np.log(y + 1)
 
 # Hold-Out
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    shuffle=True,
-                                                    train_size=0.85,
-                                                    random_state=42,
-                                                    stratify=X['smoker'])
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, shuffle=True, train_size=0.85, random_state=42, stratify=X["smoker"]
+)
 
 # Build Pipeline
 ohe_nom = OneHotEncoder(drop="first", handle_unknown="ignore")
@@ -64,15 +59,14 @@ ohe_bin = OneHotEncoder(drop="if_binary", handle_unknown="ignore")
 poly = PolynomialFeatures(degree=2)
 std = StandardScaler()
 
-en = ElasticNet(random_state=42, max_iter=10_000, tol=1e-3
-)
+en = ElasticNet(random_state=42, max_iter=10_000, tol=1e-3)
 
 encoder = ColumnTransformer(
     transformers=[
         ("bin", ohe_bin, ["sex", "smoker"]),
-        ("nom", ohe_nom, ["bmi", "region"])
+        ("nom", ohe_nom, ["bmi", "region"]),
     ],
-    remainder="passthrough"
+    remainder="passthrough",
 )
 
 model = make_pipeline(encoder, poly, std, en)
@@ -83,25 +77,14 @@ X_bmi_nom.bmi = X_bmi_nom.bmi.apply(split_bmi_in_three)
 
 # Stratified (smoker) Hold-Out
 X_bmi_nom_train, X_bmi_nom_test, y_train, y_test = train_test_split(
-    X_bmi_nom, y,
-    shuffle=True,
-    train_size=0.85,
-    random_state=42,
-    stratify=X['smoker']
+    X_bmi_nom, y, shuffle=True, train_size=0.85, random_state=42, stratify=X["smoker"]
 )
 
 # Train Model
-params = {
-    "elasticnet__alpha": uniform(0, 2),
-    "elasticnet_l1_ratio": uniform(0, 1)
-}
+params = {"elasticnet__alpha": uniform(0, 2), "elasticnet_l1_ratio": uniform(0, 1)}
 
 random_search = RandomizedSearchCV(
-    model,
-    param_distributions=params,
-    n_iter=2_000,
-    cv=5,
-    n_jobs=-1
+    model, param_distributions=params, n_iter=2_000, cv=5, n_jobs=-1
 )
 
 random_search.fit(X_bmi_nom_train, y_train)
